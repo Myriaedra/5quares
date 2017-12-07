220791756f490361dae85cbc785ed9f68b2bbcf3
charlotteAuger <auger.cha@gmail.com>
2017-12-07 21:04:27 +0100

    Beautiful portal
    activates particles when player on the portal

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 	public GameObject[] portals = new GameObject[5];
 	public string[] keys = new string[]{"space", "s", "e","r","t"};
+	public GameObject currentPortal;
 	// Use this for initialization
 	void Start () {
 		
@@ -17,7 +18,13 @@ public class PlayerController : MonoBehaviour {
 			if (Input.GetKeyDown(keys[i]))
 			{
 				Vector3 portalPosition = portals [i].transform.position;
+				currentPortal.GetComponent<PortalParticleControl> ().isOn = false;
+				currentPortal.GetComponent<PortalParticleControl> ().UpdateParticles ();
+
 				transform.position = new Vector3 (portalPosition.x, transform.position.y, portalPosition.z);
+				currentPortal = portals [i];
+				portals [i].GetComponent<PortalParticleControl> ().isOn = true;
+				portals [i].GetComponent<PortalParticleControl> ().UpdateParticles ();
 			}
 		}
 	}
---
+++ Assets/Scripts/PortalParticleControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalParticleControl : MonoBehaviour {
+	public bool isOn;
+	public GameObject portalParticle;
+	// Use this for initialization
+	void Start()
+	{
+		UpdateParticles ();
+	}
+
+	public void UpdateParticles()
+	{
+		portalParticle.SetActive (isOn);
+	}
+}
